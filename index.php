4fd0cd5f805fc7eb4ddff7c5385a424071625344
astutimega <megaputri141@gmail.com>
2021-02-26 14:38:18 +0700

    menambahkan navbar(dasar)

--- index.php
+++ index.php
@@ -7,7 +7,53 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
+
 <body>
+     <!-- navbar -->
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+           <!-- Image and text -->
+        <nav class="navbar navbar-light bg-light">
+             <div class="container-fluid">
+                   <a class="navbar-brand" href="#">
+                          <img src="img/1navbar.jpg" alt="" width="30" height="24" class="d-inline-block align-top">
+               FAMEAL
+                </a>
+        </div>
+        </nav>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll nav justify-content-end ms-auto"
+                    style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Food</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Drink</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Desserts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Snacks</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+
+                </ul>
+                </li>
+
+                </ul>
+     
+            </div>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
     
     <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
